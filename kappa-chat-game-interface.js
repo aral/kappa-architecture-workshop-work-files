2da3e5457c4323ed891a1d9cfef0f6feb37f0124
Aral Balkan <aral@ind.ie>
2018-12-18 20:58:13 +0000

    Move position update code into callback

--- kappa-chat-game-interface.js
+++ kappa-chat-game-interface.js
@@ -283,22 +283,24 @@ core.ready(['chats', 'players'], function() {
 
     const updatePosition = (deltaX = 0, deltaY = 0) => {
 
-      myX += deltaX
-      myY += deltaY
-
-      // Wrap around if necessary.
-      if (myX > termWidth - 1) myX = 1
-      if (myX < 1) myX = termWidth - 1
-      if (myY > termHeight - textAreaHeight - otherAreaHeight - 1) myY = 2
-      if (myY < 2) myY = termHeight - textAreaHeight - otherAreaHeight - 1
-
       // TODO: The player should be initialised here if non-existent.
       core.api.players.get(myId, (error, values) => {
+
+        myX += deltaX
+        myY += deltaY
+
+        // Wrap around if necessary.
+        if (myX > termWidth - 1) myX = 1
+        if (myX < 1) myX = termWidth - 1
+        if (myY > termHeight - textAreaHeight - otherAreaHeight - 1) myY = 2
+        if (myY < 2) myY = termHeight - textAreaHeight - otherAreaHeight - 1
+
         let link = feed.key.toString('hex')
         if (error === null) {
           // Set the link to update the latest local value.
           link = `${link}@${values[values.length-1].seq}`
         }
+
         feed.append({
           type: 'movement-message',
           id: myId,
